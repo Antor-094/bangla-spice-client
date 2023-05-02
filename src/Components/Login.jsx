0bdb98e8ba36e8a9828de0ce7fa3e8c8d6daf135
Antor-094 <mdantor556677889@gmail.com>
2023-05-02 11:37:28 +0600

    create user and login user added

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import backgroundImage from '../assets/foods_images/istockphoto-1152493500-1024x1024.jpg';
 import { FaGithub,FaFacebook,FaGoogle } from 'react-icons/fa';
+import { AuthContext } from '../provider/AuthProvider';
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { signIn } = useContext(AuthContext);
+  const [error,setError] = useState('')
+  const handleLogin = (e)=>{
+
+    e.preventDefault();
+    setError('')
+    if ((email && password)) {
+      signIn(email, password)
+        .then((result) => {
+          console.log(result.user);
+          // navigate("/");
+          
+          e.target.reset();
+        })
+        .catch((error) => {
+          console.log(error.message);
+          setError(error.message)
+        });
+    }
+  }
     return (
         <div className="flex items-center justify-center mt-20 p-2">
         <div className="flex flex-col md:flex-row md:w-1/2 rounded-lg overflow-hidden shadow-lg">
@@ -22,10 +45,12 @@ const Login = () => {
                   Email
                 </label>
                 <input
+                onChange={(e) => setEmail(e.target.value)}
                   className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="email"
                   type="email"
                   placeholder="Email"
+                  required
                 />
               </div>
               <div className="mb-6">
@@ -33,14 +58,17 @@ const Login = () => {
                   Password
                 </label>
                 <input
+                 onChange={(e) => setPassword(e.target.value)}
                   className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                   id="password"
                   type="password"
                   placeholder="Password"
+                  required
                 />
               </div>
               <div className="items-center justify-between">
                 <button
+                onClick={handleLogin}
                   className="bg-[#488b8f] hover:bg-[#add2c9] w-full text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                   type="button"
                 >
@@ -54,6 +82,7 @@ const Login = () => {
               <div className='flex items-center justify-evenly bg-[#faf9f9] p-4'>
                    <span ><FaGoogle></FaGoogle></span> <span><FaFacebook></FaFacebook></span> <span><FaGithub/></span>
               </div>
+              <p>{error}</p>
             </form>
           </div>
         </div>
--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import backgroundImage from '../assets/foods_images/istockphoto-1152493500-1024x1024.jpg';
-import { FaGithub,FaFacebook,FaGoogle } from 'react-icons/fa';
+import { AuthContext } from '../provider/AuthProvider';
+
 
 const Register = () => {
+
+    // const { registerUser } = useContext(AuthContext);
+
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [photo,setPhoto] = useState('')
+    const [error, setError] = useState("");
+    const { createUser } = useContext(AuthContext);
+    const handleRegistration = (event) => {
+        
+      event.preventDefault();
+      setError('')
+      if (password.length < 6) {
+        setError("The password should be less than 6 characters");
+        return;
+      }
+      createUser(email, password)
+      .then(result => {
+          const loggedUser = result.user;
+          console.log(loggedUser)
+      })
+      .catch(error => {
+          console.log(error);
+          setError(error.message);
+      })
+    };
     return (
         <div className="flex items-center justify-center mt-20 p-2">
         <div className="flex flex-col md:flex-row md:w-1/2 rounded-lg overflow-hidden shadow-lg">
@@ -22,10 +50,13 @@ const Register = () => {
                   Name
                 </label>
                 <input
+                  onChange={(e) => setName(e.target.value)}
                   className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="name"
                   type="text"
                   placeholder="Name"
+                  required
+                  
                 />
               </div>
               <div className="mb-4">
@@ -33,10 +64,12 @@ const Register = () => {
                   Email
                 </label>
                 <input
+                onChange={(e) => setEmail(e.target.value)}
                   className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="email"
                   type="email"
                   placeholder="Email"
+                  required
                 />
               </div>
               <div className="mb-6">
@@ -44,10 +77,12 @@ const Register = () => {
                   Password
                 </label>
                 <input
+                onChange={(e) => setPassword(e.target.value)}
                   className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                   id="password"
                   type="password"
                   placeholder="Password"
+                  required
                 />
               </div>
               <div className="mb-6">
@@ -55,16 +90,18 @@ const Register = () => {
                   PhotoURL
                 </label>
                 <input
+                onChange={(e) => setPhoto(e.target.value)}
                   className="appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                   id="photo"
                   type="text"
                   placeholder="photo url"
+                  required
                 />
               </div>
               <div className="items-center justify-between">
                 <button
                   className="bg-[#488b8f] hover:bg-[#add2c9] w-full text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  type="button"
+                  type="button" onClick={handleRegistration}
                 >
                   Sign Up
                 </button>
@@ -72,7 +109,7 @@ const Register = () => {
                    already have an account?<Link className='inline-block align-baseline font-bold text-sm #488b8f hover:text-[#add2c9]' to='/login'>Login!!</Link>
                 </p>
               </div>
-             
+              <p className='text-error'>{error}</p>
             </form>
           </div>
         </div>
